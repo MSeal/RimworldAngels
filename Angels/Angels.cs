6c9f93cb22b2bb0ecceee30aa2051674507882ba
Matthew Seal <mseal007@gmail.com>
2018-09-23 16:40:03 -0700

    Added initial implementation

---
+++ Angels/Angels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Angels
+{
+    // TODO: Move to backstory and remove
+    class DefModExtension_AngelPawn : DefModExtension {
+        public List<string> restrictedFirstNames;
+        public List<string> restrictedLastNames;
+    }
+
+    public class Angels
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RimworldAngels
 A mod which add an "Angel" cannibal gang faction to the game
+
+## Credits
+[Angel Logo Designed by Freepik](https://www.freepik.com/index.php?goto=74&idfoto=1141430)
